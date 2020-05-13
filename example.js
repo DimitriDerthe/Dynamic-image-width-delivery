93944e707067ebf8d978c187fc75976c725a7d01
Dimitri DERTHE <dimitri.derthe@gmail.com>
2020-05-13 12:32:30 +0200

    add: add demo script

---
+++ example.js
@@ -0,0 +1,27 @@
+//Define our standard image assets, here 20 different resolutions
+//Can be automated by bucket listing and memory cache for performance
+var array = ['50', '261', '385', '493', '578', '659', '736', '805', '872', '940', '1002', '1062', '1127', '1185', '1242', '1311', '1367', '1425', '1433', '1440']
+
+//Function finding the closest resolution for the user
+function closestRes(array,num){
+    console.log('Res. to check:', num)
+    var i=0
+    var minDiff=1000
+    var ans=null
+    for(i in array){
+        var m=Math.abs(num-array[i])
+        if(m<minDiff){ 
+            minDiff=m
+            ans=array[i]
+        }  
+    }
+    if(!ans){
+        //If can't find best resolution, return highest resolution
+        ans=array[array.length-1]
+        console.log('Return max def.', ans)
+    }
+    console.log('Returned resolution:', ans)
+    return ans
+}
+
+closestRes(array, document.getElementById(id_image).clientWidth);
